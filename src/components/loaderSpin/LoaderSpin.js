9cac166aa7b3867119faa7c7ae264f452b5c4364
fordrodenberg <110043501+fordrodenberg@users.noreply.github.com>
2022-09-28 14:12:09 -0400

    Added message in loading screen

--- src/components/loaderSpin/LoaderSpin.js
+++ src/components/loaderSpin/LoaderSpin.js
@@ -7,10 +7,12 @@ export default function LoaderSpin() {
             <div className='blank-space'></div>
             <div className="circle">
             </div>
+            <div className='map-loading'>Loading Map...</div>
             <div className='logo'>chuck
                 <span className='dot'>.</span>
                 <span className='spot-logo'>Spot</span>
             </div>
+
         </div>
     )
 }
